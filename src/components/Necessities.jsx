179c37e61b6a6cf542b5255643bb33a40846489e
Baaba <baabadampare@gmail.com>
2022-07-21 16:17:35 +0000

    add pseudo element to necessities component

--- src/components/Necessities.jsx
+++ src/components/Necessities.jsx
@@ -5,17 +5,17 @@ const Necessities = () => {
   return (
     <div className="bg-bg-off flex flex-col justify-center items-center pt-24">
       <div>
-        <h3 className="font-header font-bold text-primary-b text-center text-4xl">
+        <h3 className="font-header font-bold text-primary-b text-center text-4xl after:content-[url('./assets/TwoPlanes.png')] after:block after:absolute after:right-56">
           Things you need <span className="text-primary-o">to do</span>
         </h3>
-        <p className='text-text-light font-text text-center text-lg w-[30rem] pt-10 pb-14'>
+        <p className="text-text-light font-text text-center text-lg w-[30rem] pt-10 pb-14">
           We ensure that you’ll embark on a perfectly planned, safe vacation at
           a price you can afford.
         </p>
       </div>
 
       <div>
-        <NecessaryCards/>
+        <NecessaryCards />
       </div>
     </div>
   );
