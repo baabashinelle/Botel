02acdd740506d19841b59698f48b9655813a2f6d
Baaba <baabadampare@gmail.com>
2022-07-22 14:13:53 +0000

    update necessities pseudo element

--- src/components/Necessities.jsx
+++ src/components/Necessities.jsx
@@ -3,9 +3,9 @@ import NecessaryCards from '../components/NecessaryCards';
 
 const Necessities = () => {
   return (
-    <div className="bg-bg-off flex flex-col justify-center items-center pt-24">
+    <div className="bg-bg-off flex flex-col justify-center items-center pt-24 pb-20">
       <div>
-        <h3 className="font-header font-bold text-primary-b text-center text-4xl after:content-[url('./assets/TwoPlanes.png')] after:block after:absolute after:right-56">
+        <h3 className="font-header font-bold text-primary-b text-center text-4xl after:content-[url('./assets/TwoPlanes.png')] after:block after:absolute after:right-56 after:-bottom-[30rem]">
           Things you need <span className="text-primary-o">to do</span>
         </h3>
         <p className="text-text-light font-text text-center text-lg w-[30rem] pt-10 pb-14">
