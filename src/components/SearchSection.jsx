fa4a64556bcf1efb2809fa228aad8bb7301ea5ca
Baaba <baabadampare@gmail.com>
2022-08-29 16:08:19 +0000

    semantic tags in search component

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -5,32 +5,32 @@ const SearchSection = () => {
   return (
     <div className="flex justify-center items-center mt-[5rem] mb-[8rem]">
       <div className="bg-white flex justify-center items-center rounded-md font-text gap-20 w-[55rem] py-6">
-        <div>
+        <article>
           <div className="flex items-center">
             <h6 className="text-primary-b font-semibold">Location</h6>
             <MdOutlineKeyboardArrowDown className="text-primary-o text-xl" />
           </div>
           <p className="text-text-light text-sm pt-3">Where are you going</p>
-        </div>
-        <div>
+        </article>
+        <article>
           <div className="flex items-center">
             <h6 className="text-primary-b font-semibold">Date</h6>
             <MdOutlineKeyboardArrowDown className="text-primary-o text-xl" />
           </div>
           <p className="text-text-light text-sm pt-3">When you will go</p>
-        </div>
-        <div>
+        </article>
+        <article>
           <div className="flex items-center">
             <h6 className="text-primary-b font-semibold">Guest</h6>
             <MdOutlineKeyboardArrowDown className="text-primary-o text-xl" />
           </div>
           <p className="text-text-light text-sm pt-3">Number of guest</p>
-        </div>
-        <div>
+        </article>
+        <article>
           <button className="w-[10rem] p-3 font-text rounded-md text-white font-semibold bg-primary-o border-primary-o border-[1px] drop-shadow-xl">
             Explore Now
           </button>
-        </div>
+        </article>
       </div>
     </div>
   );
