7a3b4a7c9ea106ada8f9f20da709f94bacb9f45b
Baaba <baabadampare@gmail.com>
2022-08-29 20:08:22 +0000

    add hover effects to hotel cards

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
-import Hotels from './Hotels';
+import Hotels from './Hotels'
+import { GrFormPreviousLink, GrFormNextLink } from "react-icons/gr";
+
 
 const Deals = () => {
   return (
     <div className="bg-bg-c flex flex-col justify-center items-center pt-24 pb-20">
       <article>
         <h3 className="font-header font-bold text-primary-b text-center text-4xl">
-          Exclusive <span className="text-primary-o">deals &amp; discounts</span>
+          Exclusive{" "}
+          <span className="text-primary-o">deals &amp; discounts</span>
         </h3>
         <p className="text-text-light font-text text-center text-lg w-[30rem] pt-10 pb-14">
-          Discover our fantastic early booking discounts &amp; start planning your journey.
+          Discover our fantastic early booking discounts &amp; start planning
+          your journey.
         </p>
       </article>
-
       <article>
-        <Hotels/>
+        <Hotels />
+      </article>
+      <article className="flex pt-10 text-3xl gap-3">
+        <div className="border-2 rounded-full">
+          <GrFormPreviousLink/>
+        </div>
+        <div>
+          <GrFormNextLink />
+        </div>
       </article>
     </div>
   );
