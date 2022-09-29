f782af562e115f18c2a2d3f4bf7ad15a3ab43ea1
Baaba <baabadampare@gmail.com>
2022-09-29 22:14:32 +0000

    semantic tags

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className="bg-bg-c flex flex-col justify-center items-center pt-24 pb-20">
+    <section className="bg-bg-c flex flex-col justify-center items-center pt-24 pb-20">
       <article>
         <h3 className="font-header font-bold text-primary-b text-center text-4xl">
           Get update with
@@ -12,7 +12,7 @@ const Blog = () => {
       </article>
       <article className='pt-8'>
       </article>
-    </div>
+    </section>
   );
 }
 
