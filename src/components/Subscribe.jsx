a49d1307cffd8ebdbfd682d0439ec048bcf91ae2
Baaba <baabadampare@gmail.com>
2022-09-29 23:18:27 +0000

    add subscribe component

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -5,7 +5,8 @@ const Subscribe = () => {
     <section className="flex flex-col justify-center items-center pt-24 pb-20 font-text text-white">
       <div className="bg-primary-o py-20 px-16 rounded-lg">
         <h2 className='text-3xl font-bold font-header'>Subscribe and get exclusive deals &amp; offer</h2>
-        <input type='text'></input>
+        <input type='text'>
+        </input>
       </div>
     </section>
   );
