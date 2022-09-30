6b1f01f29e9c9cb2ba9d270ccb0fd0c1b511529e
Baaba <baabadampare@gmail.com>
2022-09-30 23:02:36 +0000

    update subscribe component

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <section className="flex flex-col justify-center items-center pt-24 pb-20 font-text text-white">
-      <div className="bg-primary-o py-20 px-16 rounded-lg">
-        <h2 className='text-3xl font-bold font-header'>Subscribe and get exclusive deals &amp; offer</h2>
+      <div className="bg-primary-o py-20 px-24 rounded-lg">
+        <h2 className='text-3xl font-bold font-header text-center w-[25ch]'>Subscribe and get exclusive deals &amp; offer</h2>
         <input type='text'>
         </input>
       </div>
