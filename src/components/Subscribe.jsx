5133a4d8e314baf4990709ac6d3bcfe1b70f9b89
Baaba <baabadampare@gmail.com>
2022-09-30 23:12:58 +0000

    add white trees img

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <section className="flex flex-col justify-center items-center pt-24 pb-20 font-text text-white">
-      <div className="bg-primary-o py-20 px-24 rounded-lg">
-        <h2 className='text-3xl font-bold font-header text-center w-[25ch]'>Subscribe and get exclusive deals &amp; offer</h2>
-        <input type='text'>
-        </input>
+      <div className="bg-primary-o py-20 px-24 rounded-lg relative before:content-[url('./assets/trees.png')] before:block before:absolute before:right-0 before:top-0">
+        <h2 className="text-3xl font-bold font-header text-center w-[25ch]">
+          Subscribe and get exclusive deals &amp; offer
+        </h2>
+        <input type="text"></input>
       </div>
     </section>
   );
