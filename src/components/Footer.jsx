a9675af35aa0384ab1cbb49ace4a28542eaff038
Baaba <baabadampare@gmail.com>
2022-10-01 02:07:33 +0000

    add lower footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,61 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/logo.png";
-import {FaFacebookF} from 'react-icons/fa';
-import { BsInstagram, BsTwitter } from "react-icons/bs";
-
+import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="bg-bg-off flex justify-center items-center pt-24 pb-20 font-text">
-      <article className="flex flex-col gap-3">
-        <div className="flex flex-row items-center gap-2">
-          <h3 className="text-2xl font-bold font-header">Botel</h3>
-          <div>
-            <img src={logo} alt="Botel Logo" />
+    <footer className="bg-bg-off flex flex-col gap-14 pt-24 pb-14 font-text">
+      <section className="flex justify-center">
+        <article className="flex flex-col gap-3 basis-1/3">
+          <div className="flex flex-row items-center gap-2">
+            <h3 className="text-2xl font-bold font-header">Botel</h3>
+            <div>
+              <img src={logo} alt="Botel Logo" />
+            </div>
           </div>
-        </div>
-        <p className="w-[30ch]">
-          Book your stay in minutes, get full control for much longer.
-        </p>
-        <div className="flex gap-5">
-          <div className='bg-white text-black rounded-full'>
-            <FaFacebookF />
+          <p className="w-[30ch]">
+            Book your stay in minutes, get full control for much longer.
+          </p>
+          <div className="flex gap-5">
+            <FaFacebookF className="bg-white rounded-full p-3 text-black drop-shadow-md" />
+            <BsInstagram className="bg-white rounded-full p-3 text-black drop-shadow-md" />
+            <AiOutlineTwitter className="bg-white rounded-full p-3 text-black drop-shadow-md" />
           </div>
-          <BsInstagram />
-          <BsTwitter />
+        </article>
+        <div className="flex gap-16">
+          <article className="flex flex-col gap-3 text-gray-400">
+            <h3 className="font-bold pb-2 text-black">Company</h3>
+            <p>About</p>
+            <p>Careers</p>
+            <p>Logistic</p>
+            <p>Privacy Policy</p>
+          </article>
+          <article className="flex flex-col gap-3 text-gray-400">
+            <h3 className="font-bold pb-2 text-black">Contact</h3>
+            <p>Help/FAQ</p>
+            <p>Press</p>
+            <p>Affilates</p>
+          </article>
+          <article className="flex flex-col gap-3 text-gray-400">
+            <h3 className="font-bold pb-2 text-black">More</h3>
+            <p>Press Centre</p>
+            <p>Our Blog</p>
+            <p>Low fare tips</p>
+          </article>
         </div>
-      </article>
+      </section>
+
+      {/* Lower Footer */}
+      <section className="flex justify-center">
+        <article className="flex justify-between text-gray-400 border-t-2">
+          <p>Copyright, Botel 2022. All rights reserved.</p>
+          <p>Terms &amp; Conditions</p>
+        </article>
+      </section>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Subscribe = () => {
   return (
     <section className="flex flex-col justify-center items-center pt-24 pb-20 bg-bg-c font-text text-white">
-      <div className="w-fit bg-primary-o py-20 px-32 rounded-lg overflow-hidden relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:content-[url('./assets/circles.png')] after:block after:w-10 after:absolute after:left-0 after:-bottom-2">
+      <div className="w-fit bg-primary-o py-24 px-[14rem] rounded-lg overflow-hidden relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:content-[url('./assets/circles.png')] after:block after:w-10 after:absolute after:left-0 after:-bottom-2">
         <h2 className="text-3xl font-bold font-header text-center w-[25ch]">
           Subscribe and get exclusive deals &amp; offer
         </h2>
