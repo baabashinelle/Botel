d13cbab7ff47bd29cf32c7d5b4f72c38f41d21f2
Baaba <baabadampare@gmail.com>
2022-10-01 00:11:31 +0000

    update subscribe component

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Subscribe = () => {
   return (
     <section className="flex flex-col justify-center items-center pt-24 pb-20 font-text text-white">
-      <div className="bg-primary-o py-20 px-28 rounded-lg overflow-hidden relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:content-[url('./assets/circles.png')] after:block after:absolute after:left-0 after:-bottom-2 after:w-[0rem]">
+      <div className="w-fit bg-primary-o py-20 px-32 rounded-lg overflow-hidden relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:content-[url('./assets/circles.png')] after:block after:w-10 after:absolute after:left-0 after:-bottom-2">
         <h2 className="text-3xl font-bold font-header text-center w-[25ch]">
           Subscribe and get exclusive deals &amp; offer
         </h2>
-        <input type="text"></input>
       </div>
     </section>
   );
-}
+};
 
-export default Subscribe
+export default Subscribe;
