2286bbbda3361f43ced9a2dacf47e512585a1320
Baaba <baabadampare@gmail.com>
2022-10-04 01:04:23 +0000

    updates

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -4,7 +4,7 @@ import { BsEnvelope } from "react-icons/bs";
 const Subscribe = () => {
   return (
     <section className="flex flex-col justify-center items-center -mb-[11rem] pt-24 bg-bg-c font-text text-white">
-      <div className="w-fit bg-primary-o py-24 px-[14rem] rounded-lg overflow-hidden relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:content-[url('./assets/circles.png')] after:block after:w-10 after:absolute after:left-0 after:-bottom-2">
+      <div className="w-fit bg-primary-o py-24 px-[14rem] rounded-lg relative before:content-[url('./assets/trees-white.png')] before:block before:absolute before:right-0 before:top-[0.5rem] after:bg-[url('./assets/circles.png')] after:w-[8rem] after:h-[8rem] after:block after:absolute after:left-0 after:bottom-10">
         <h2 className="text-3xl font-bold font-header text-center w-[25ch] tracking-wide">
           Subscribe and get exclusive deals &amp; offer
         </h2>
