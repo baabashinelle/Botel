70716a6364119d7d449cc2d9e44b8c26f197cb26
Baaba <baabadampare@gmail.com>
2022-10-06 13:18:34 +0000

    edit routing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Home from './pages/Home';
 function App() {
   return (
     <div>
-        <Home />
-        <Routes>
-          <Route exact path="/dashboard" element={<Dashboard />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
     </div>
   );
 }
