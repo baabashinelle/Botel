83ec325ca8fb0cf9935d05a19529a9ecd49d15f8
Baaba <baabadampare@gmail.com>
2022-10-06 14:39:20 +0000

    start dashboard sidebar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,12 @@ import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineAddBusiness } from "react-icons/md";
-import { GrOverview } from "react-icons/gr";
+import { AiOutlineEye } from "react-icons/ai";
+import { FiLogOut } from "react-icons/fi";
 
 const Dashboard = () => {
   return (
-    <section className="w-[20%] bg-red-300 min-h-screen flex flex-col items-center font-text p-5 shadow-sm">
+    <section className="w-[20%] min-h-screen bg-bg-c flex flex-col items-center font-text p-5 shadow-sm">
       {/*logo*/}
       <article className="flex justify-center items-center gap-2 w-full mb-[6rem]">
         <h3 className="text-2xl font-bold font-header">Botel</h3>
@@ -16,21 +17,34 @@ const Dashboard = () => {
         </div>
       </article>
       <article>
-        <ul className="gap-5 flex flex-col">
-          <Link className="flex items-center gap-4">
-            <AiOutlineHome />
-            Dashboard
-          </Link>
-          <Link className="flex items-center gap-4">
-            <MdOutlineAddBusiness />
-            Add Hotels
-          </Link>
-          <Link className="flex items-center gap-4">
-            <GrOverview />
-            View Hotels
-          </Link>
+        <ul className="gap-5 flex flex-col text-primary-o text-lg">
+          <li className="bg-[#ffe3d4] px-8 py-2 rounded-md">
+            <Link className="flex items-center gap-4">
+              <AiOutlineHome />
+              Dashboard
+            </Link>
+          </li>
+          <li className="px-8 py-2 rounded-md">
+            <Link className="flex items-center gap-4">
+              <MdOutlineAddBusiness />
+              Add Hotels
+            </Link>
+          </li>
+          <li className="px-8 py-2 rounded-md">
+            <Link className="flex items-center gap-4">
+              <AiOutlineEye />
+              View Hotels
+            </Link>
+          </li>
         </ul>
       </article>
+      {/* <article>
+        <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg">
+          <Link to="/" className="flex items-center gap-4">
+            <FiLogOut /> Logout
+          </Link>
+        </button>
+      </article> */}
     </section>
   );
 };
