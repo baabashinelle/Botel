adaa0ab92e719e2c36a4d14ee714ff35d7778515
Baaba <baabadampare@gmail.com>
2022-10-06 14:57:49 +0000

    insert nav dashboard

---
+++ src/components/DashboardNav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import avatarImg from "../assets/avatar.jpg";
+
+const DashboardNav = () => {
+  return (
+    <nav className="w-full flex font-text text-lg py-4 px-6 justify-end items-center bg-bg-c">
+      <div className="flex items-center justify-center gap-3">
+        <div className="text-right flex-col flex items-end justify-center">
+          <h3 className="font-extrabold">Howdy!</h3>
+          <p className="text-sm text-gray-600">User</p>
+        </div>
+        <div className="relative w-12 h-12">
+          <img
+            src={avatarImg}
+            alt="admin"
+            className="h-full w-full rounded-full"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default DashboardNav
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SideBar from "../components/SideBar";
+import DashboardNav from "../components/DashboardNav"
 
 const Dashboard = () => {
   return (
-    <>
+    <main className="flex h-screen items-start overflow-hidden">
       <SideBar />
-    </>
+      <DashboardNav />
+    </main>
   );
 };
 
