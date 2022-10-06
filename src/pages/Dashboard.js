e636a0d2fbe443afc4953f7094c96e2e92fa9cea
Baaba <baabadampare@gmail.com>
2022-10-06 14:03:39 +0000

    start dashboard sidebar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineAddBusiness } from "react-icons/md";
+import { GrOverview } from "react-icons/gr";
 
 const Dashboard = () => {
   return (
-    <div>
-        dashboard
-    </div>
-  )
-}
+    <section className="w-[20%] bg-red-300 min-h-screen flex flex-col items-center font-text p-5 shadow-sm">
+      {/*logo*/}
+      <article className="flex justify-center items-center gap-2 w-full mb-[6rem]">
+        <h3 className="text-2xl font-bold font-header">Botel</h3>
+        <div>
+          <img src={logo} alt="Botel Logo" />
+        </div>
+      </article>
+      <article>
+        <ul className="gap-5 flex flex-col">
+          <Link className="flex items-center gap-4">
+            <AiOutlineHome />
+            Dashboard
+          </Link>
+          <Link className="flex items-center gap-4">
+            <MdOutlineAddBusiness />
+            Add Hotels
+          </Link>
+          <Link className="flex items-center gap-4">
+            <GrOverview />
+            View Hotels
+          </Link>
+        </ul>
+      </article>
+    </section>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
