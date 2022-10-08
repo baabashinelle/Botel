8034ed6f2515e05df6ee044b0a35e444a4838c7b
Baaba <baabadampare@gmail.com>
2022-10-08 22:30:50 +0000

    insert hotels

--- src/utils/hotelsTable.jsx
+++ src/utils/hotelsTable.jsx
@@ -2,25 +2,25 @@ const HotelItems = [
   {
     name: "Hotel de France",
     country: "France",
-    hotelImg: "/../assets/hotel1.png",
+    hotelImg: "../assets/hotel1.png",
     status: "Available",
   },
   {
     name: "Kempinski Hotel",
     country: "Ghana",
-    hotelImg: "/../assets/hotel2.png",
+    hotelImg: "../assets/hotel2.png",
     status: "Unavailable",
   },
   {
     name: "Transcorp Hilton",
     country: "Nigeria",
-    hotelImg: "/../assets/hotel3.png",
+    hotelImg: "../assets/hotel3.png",
     status: "Available",
   },
   {
     name: "The Ritz-Carlton",
     country: "Canada",
-    hotelImg: "/../assets/hotel4.png",
+    hotelImg: "../assets/hotel4.png",
     status: "Unavailable",
   },
 ];
