53d3b98ce8f4205d380fe60a9d826d77de3edd21
Baaba <baabadampare@gmail.com>
2022-10-08 21:45:30 +0000

    add utils folder

---
+++ src/utils/hotelsTable.jsx
@@ -0,0 +1,28 @@
+const HotelItems = [
+  {
+    name: "Miss Agriculture",
+    date: "14 April, 2022",
+    eventImg: "/assets/events.jpg",
+    status: "Open",
+  },
+  {
+    name: "Miss World",
+    date: "13 April, 2022",
+    eventImg: "/assets/events2.jpg",
+    status: "Open",
+  },
+  {
+    name: "Miss Universe",
+    date: "10 April, 2022",
+    eventImg: "/assets/events.jpg",
+    status: "Closed",
+  },
+  {
+    name: "Miss Malaika",
+    date: "18 April, 2022",
+    eventImg: "/assets/events2.jpg",
+    status: "Closed",
+  },
+];
+
+export default HotelItems;
