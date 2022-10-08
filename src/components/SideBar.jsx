f554baf9c70adb23604f43f12f3d0c220b89e8a3
Baaba <baabadampare@gmail.com>
2022-10-08 11:58:23 +0000

    updates

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineAddBusiness } from "react-icons/md";
 import { AiOutlineEye } from "react-icons/ai";
@@ -16,7 +17,9 @@ const SideBar = ({ setPage }) => {
     <section className="w-[20%] min-h-screen flex flex-col items-center font-text p-5 shadow-sm">
       {/*logo*/}
       <article className="flex justify-center items-center gap-2 w-full mb-[6rem]">
-        <h3 className="text-2xl font-bold font-header">Botel</h3>
+        <h3 className="text-2xl font-bold font-header">
+          <Link to="/">Botel</Link>
+        </h3>
         <div>
           <img src={logo} alt="Botel Logo" />
         </div>
