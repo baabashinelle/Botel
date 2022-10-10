4f584eec3fe57ab4426be6cba4f31abcafa57ef6
Baaba <baabadampare@gmail.com>
2022-10-10 21:15:11 +0000

    add rooms table in utils folder

---
+++ src/utils/roomsTable.jsx
@@ -0,0 +1,28 @@
+const RoomItems = [
+  {
+    name: "Hotel de France",
+    country: "France",
+    hotelImg: "../assets/hotel1.png",
+    status: "Available",
+  },
+  {
+    name: "Kempinski Hotel",
+    country: "Ghana",
+    hotelImg: "../assets/hotel2.png",
+    status: "Unavailable",
+  },
+  {
+    name: "Transcorp Hilton",
+    country: "Nigeria",
+    hotelImg: "../assets/hotel3.png",
+    status: "Available",
+  },
+  {
+    name: "The Ritz-Carlton",
+    country: "Canada",
+    hotelImg: "../assets/hotel4.png",
+    status: "Unavailable",
+  },
+];
+
+export default RoomItems;
