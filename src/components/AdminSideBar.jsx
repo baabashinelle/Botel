affae5d548e8bcca50c49166590e6b39a3ac91d9
Baaba <baabadampare@gmail.com>
2022-10-10 21:06:19 +0000

    add admin sidebar

--- src/components/AdminSideBar.jsx
+++ src/components/AdminSideBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineAddBusiness } from "react-icons/md";
+import { AiOutlineEye } from "react-icons/ai";
+import AddHotel from "./AddHotel";
+import Tabs from "./Tabs";
+import ViewHotels from "./ViewHotels";
 
 const AdminSideBar = () => {
   return (
-    <div>AdminSideBar</div>
+    <div></div>
   )
 }
 
