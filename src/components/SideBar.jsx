2b6703b55c55a76d40a50efdec65781b1262bd74
Baaba <baabadampare@gmail.com>
2022-10-10 21:09:58 +0000

    edit sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
-import { MdOutlineAddBusiness } from "react-icons/md";
 import { AiOutlineEye } from "react-icons/ai";
 import AddHotel from "./AddHotel";
 import Tabs from "./Tabs";
