ea12992563fb65c34accff42cfbceb0059867785
Baaba <baabadampare@gmail.com>
2022-10-12 13:28:07 +0000

    make necessities responsive

--- src/components/Necessities.jsx
+++ src/components/Necessities.jsx
@@ -5,10 +5,10 @@ const Necessities = () => {
   return (
     <section className="bg-bg-off flex flex-col justify-center items-center pt-24 pb-20">
       <article>
-        <h3 className="font-header font-bold text-primary-b text-center text-4xl relative after:content-[url('./assets/TwoPlanes.png')] after:block after:absolute after:left-[15em] after:-top-[0.3em]">
+        <h3 className="font-header font-bold text-primary-b text-center md:text-4xl text-3xl relative md:after:content-[url('./assets/TwoPlanes.png')] after:block after:absolute after:left-[15em] after:-top-[0.3em]">
           Things you need <span className="text-primary-o">to do</span>
         </h3>
-        <p className="text-text-light font-text text-center text-lg w-[30rem] pt-10 pb-14">
+        <p className="text-text-light font-text text-center text-lg md:w-[30rem] w-[25rem] pt-10 pb-14">
           We ensure that you’ll embark on a perfectly planned, safe vacation at
           a price you can afford.
         </p>
