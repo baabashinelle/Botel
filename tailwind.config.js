c41f91f07eb4daeba6e1a5269494629268d6cf71
Baaba <baabadampare@gmail.com>
2022-10-12 12:46:23 +0000

    edit tailwind config fiile

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,21 @@ module.exports = {
       gridTemplateRows: {
         custom: "repeat(7, 10.5%)",
       },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
     },
   },
   plugins: [],
