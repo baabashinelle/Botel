b7feeb612e8d44848a7b852e3840cde51b517755
Baaba <baabadampare@gmail.com>
2022-10-15 04:35:32 +0000

    edit modal design

--- src/components/ViewModalDesign.jsx
+++ src/components/ViewModalDesign.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 import RoomImg from "../assets/room1.jpg";
 
 const ViewModalDesign = () => {
   return (
-    <div className="flex justify-center px-8 py-8 gap-8">
+    <div className="flex flex-col justify-center px-10 py-7">
       <article>
         <img src={RoomImg} alt="Room" />
       </article>
-      <article className="flex flex-col gap-3 mx-4">
-        <h3 className='font-bold text-lg'>Description</h3>
-        <p className='w-[18ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore autem necessitatibus.</p>
+      <article className="flex flex-col gap-2 pt-5">
+        <h3 className="font-bold text-lg">Description</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          tempore autem necessitatibus.
+        </p>
       </article>
     </div>
   );
-}
+};
 
-export default ViewModalDesign
+export default ViewModalDesign;
