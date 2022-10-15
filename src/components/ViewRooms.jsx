c73f1148e663447b5cf9e386249333a7f12c4f8a
Baaba <baabadampare@gmail.com>
2022-10-15 02:34:40 +0000

    update css

--- src/components/ViewRooms.jsx
+++ src/components/ViewRooms.jsx
@@ -21,9 +21,9 @@ const RoomItem = ({ roomImg, name, country, status }) => {
       </div>
       <div>
         {status == "Available" ? (
-          <div className="bg-green-300 text-green-700 py-1 px-3">{status}</div>
+          <div className="bg-green-200 text-green-700 py-1 px-3">{status}</div>
         ) : (
-          <div className="bg-red-300 text-red-700 py-1 px-3">{status}</div>
+          <div className="bg-red-200 text-red-700 py-1 px-3">{status}</div>
         )}
       </div>
       <div className="flex gap-4 cursor-pointer">
