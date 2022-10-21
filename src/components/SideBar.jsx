0898c3302ad982af20af30d56aac487921cf5e98
Baaba <baabadampare@gmail.com>
2022-10-21 23:43:26 +0000

    restructure sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/logo.png";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineEye } from "react-icons/ai";
-// import { FiLogOut } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
 
 const SideBar = () => {
-const { pathname } = useLocation();
+  const { pathname } = useLocation();
   return (
     <section className="w-[20%] min-h-screen flex flex-col items-center font-text p-5 shadow-sm">
       {/*logo*/}
@@ -18,7 +18,7 @@ const { pathname } = useLocation();
           <img src={logo} alt="Botel Logo" />
         </div>
       </article>
-      <article>
+      <article className="flex flex-col justify-between h-[70vh]">
         <ul className="gap-5 flex flex-col text-[#5a3f31] text-lg">
           <li
             className={`${
@@ -51,14 +51,14 @@ const { pathname } = useLocation();
             </div>
           </li>
         </ul>
+        <div>
+          <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg w-full">
+            <Link to="/" className="flex items-center justify-center gap-4">
+              <FiLogOut /> Logout
+            </Link>
+          </button>
+        </div>
       </article>
-      {/* <article>
-        <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg">
-          <Link to="/" className="flex items-center gap-4">
-            <FiLogOut /> Logout
-          </Link>
-        </button>
-      </article> */}
     </section>
   );
 };
