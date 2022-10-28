d49e77e8c32d2178be09988deae498ed4da7ca7b
Baaba <baabadampare@gmail.com>
2022-10-28 14:06:19 +0000

    edit login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,24 +5,26 @@ import { Link } from "react-router-dom";
 const LoginPage = () => {
   return (
     <div className="flex items-center gap-16 h-screen overflow-y-hidden font-text">
-      <article className="w-1/2">
+      <article className="w-4/5 h-full">
         <img src={loginImg} alt="login" />
       </article>
-      <article className="flex flex-col gap-4 w-1/4">
-        <h1 className="text-4xl font-header font-bold">Login to Botel</h1>
-        <label>Username</label>
-        <input
-          type="text"
-          className="p-2 border-2 rounded-md focus:outline-none focus:border-2 focus:border-bg-o"
-        ></input>
-        <label>Password</label>
-        <input
-          type="password"
-          className="p-2 border-2 rounded-md focus:outline-none focus:border-2 focus:border-bg-o"
-        ></input>
-        <button className="bg-primary-o text-white rounded-md py-3 mt-5">
-          <Link to="/dashboardhome">Submit</Link>
-        </button>
+      <article className='flex justify-center w-full'>
+        <article className="flex flex-col gap-4 w-1/2">
+          <h1 className="text-4xl font-header font-bold">Login to Botel</h1>
+          <label>Username</label>
+          <input
+            type="text"
+            className="p-2 border-2 rounded-md focus:outline-none focus:border-2 focus:border-bg-o"
+          ></input>
+          <label>Password</label>
+          <input
+            type="password"
+            className="p-2 border-2 rounded-md focus:outline-none focus:border-2 focus:border-bg-o"
+          ></input>
+          <button className="bg-primary-o text-white rounded-md py-3 mt-5">
+            <Link to="/dashboardhome">Submit</Link>
+          </button>
+        </article>
       </article>
     </div>
   );
