604422e202575a317acdd284a6701fcf24df1783
Baaba <baabadampare@gmail.com>
2022-10-30 19:57:18 +0000

    updates

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,14 +4,14 @@ import DashboardNav from "./DashboardNav";
 
 
 const Dashboard = ({ children }) => {
-  const [showSideBar, setShowSideBar] = useState(true);
+  const [showSideBar, setShowSideBar] = useState(false);
   const handleClick = () => {
     setShowSideBar(!showSideBar);
   }
 
   return (
     <main className="flex h-screen justify-between">
-      {showSideBar && <SideBar />}
+       <SideBar showSideBar={showSideBar}/>
       <div className="flex flex-col w-full bg-gray-100 min-h-[100vh] overflow-auto">
         <DashboardNav handleClick={handleClick} />
         {children}
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,61 +5,119 @@ import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineEye } from "react-icons/ai";
 import { FiLogOut } from "react-icons/fi";
 
-const SideBar = () => {
+const SideBar = ({ showSideBar }) => {
   const { pathname } = useLocation();
   return (
-    <section className="w-[20%] min-h-screen flex flex-col items-center font-text p-5 shadow-sm">
-      {/*logo*/}
-      <article className="flex justify-center items-center gap-2 w-full mb-[6rem] pt-2">
-        <h3 className="text-2xl font-bold font-header">
-          <Link to="/">Botel</Link>
-        </h3>
-        <div>
-          <img src={logo} alt="Botel Logo" />
-        </div>
-      </article>
-      <article className="flex flex-col justify-between h-[70vh]">
-        <ul className="gap-5 flex flex-col text-[#5a3f31] text-lg">
-          <li
-            className={`${
-              pathname == "/dashboardhome" ? "bg-[#ffe3d4]" : null
-            } px-8 py-2 rounded-md`}
-          >
-            <div className="flex items-center gap-4 cursor-pointer">
-              <AiOutlineHome />
-              <Link to={"/dashboardhome"}>Dashboard</Link>
+    <>
+      <section className="w-[20%] min-h-screen md:flex flex-col items-center font-text p-5 shadow-sm hidden">
+        {/*logo*/}
+        <article className="flex justify-center items-center gap-2 w-full mb-[6rem] pt-2">
+          <h3 className="text-2xl font-bold font-header">
+            <Link to="/">Botel</Link>
+          </h3>
+          <div>
+            <img src={logo} alt="Botel Logo" />
+          </div>
+        </article>
+        <article className="flex flex-col justify-between h-[70vh]">
+          <ul className="gap-5 flex flex-col text-[#5a3f31] text-lg">
+            <li
+              className={`${
+                pathname == "/dashboardhome" ? "bg-[#ffe3d4]" : null
+              } px-8 py-2 rounded-md`}
+            >
+              <div className="flex items-center gap-4 cursor-pointer">
+                <AiOutlineHome />
+                <Link to={"/dashboardhome"}>Dashboard</Link>
+              </div>
+            </li>
+            <li
+              className={`${
+                pathname == "/viewrooms" ? "bg-[#ffe3d4]" : null
+              } px-8 py-2 rounded-md`}
+            >
+              <div className="flex items-center gap-4 cursor-pointer">
+                <AiOutlineEye />
+                <Link to={"/viewrooms"}>View Rooms</Link>
+              </div>
+            </li>
+            <li
+              className={`${
+                pathname == "/viewhotels" ? "bg-[#ffe3d4]" : null
+              } px-8 py-2 rounded-md`}
+            >
+              <div className="flex items-center gap-4 cursor-pointer">
+                <AiOutlineEye />
+                <Link to={"/viewhotels"}>View Hotels</Link>
+              </div>
+            </li>
+          </ul>
+          <div>
+            <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg w-full">
+              <Link to="/" className="flex items-center justify-center gap-4">
+                <FiLogOut /> Logout
+              </Link>
+            </button>
+          </div>
+        </article>
+      </section>
+
+      {/* for mobile screens */}
+      {showSideBar && (
+        <section className="w-full min-h-screen flex flex-col items-center font-text p-5 shadow-sm md:hidden">
+          {/*logo*/}
+          <article className="flex justify-center items-center gap-2 w-full mb-[6rem] pt-2">
+            <h3 className="text-2xl font-bold font-header">
+              <Link to="/">Botel</Link>
+            </h3>
+            <div>
+              <img src={logo} alt="Botel Logo" />
             </div>
-          </li>
-          <li
-            className={`${
-              pathname == "/viewrooms" ? "bg-[#ffe3d4]" : null
-            } px-8 py-2 rounded-md`}
-          >
-            <div className="flex items-center gap-4 cursor-pointer">
-              <AiOutlineEye />
-              <Link to={"/viewrooms"}>View Rooms</Link>
+          </article>
+          <article className="flex flex-col justify-between h-[70vh]">
+            <ul className="gap-5 flex flex-col text-[#5a3f31] text-lg">
+              <li
+                className={`${
+                  pathname == "/dashboardhome" ? "bg-[#ffe3d4]" : null
+                } px-8 py-2 rounded-md`}
+              >
+                <div className="flex items-center gap-4 cursor-pointer">
+                  <AiOutlineHome />
+                  <Link to={"/dashboardhome"}>Dashboard</Link>
+                </div>
+              </li>
+              <li
+                className={`${
+                  pathname == "/viewrooms" ? "bg-[#ffe3d4]" : null
+                } px-8 py-2 rounded-md`}
+              >
+                <div className="flex items-center gap-4 cursor-pointer">
+                  <AiOutlineEye />
+                  <Link to={"/viewrooms"}>View Rooms</Link>
+                </div>
+              </li>
+              <li
+                className={`${
+                  pathname == "/viewhotels" ? "bg-[#ffe3d4]" : null
+                } px-8 py-2 rounded-md`}
+              >
+                <div className="flex items-center gap-4 cursor-pointer">
+                  <AiOutlineEye />
+                  <Link to={"/viewhotels"}>View Hotels</Link>
+                </div>
+              </li>
+            </ul>
+            <div>
+              <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg w-full">
+                <Link to="/" className="flex items-center justify-center gap-4">
+                  <FiLogOut /> Logout
+                </Link>
+              </button>
             </div>
-          </li>
-          <li
-            className={`${
-              pathname == "/viewhotels" ? "bg-[#ffe3d4]" : null
-            } px-8 py-2 rounded-md`}
-          >
-            <div className="flex items-center gap-4 cursor-pointer">
-              <AiOutlineEye />
-              <Link to={"/viewhotels"}>View Hotels</Link>
-            </div>
-          </li>
-        </ul>
-        <div>
-          <button className="bg-primary-o text-white rounded-md px-8 py-2 text-lg w-full">
-            <Link to="/" className="flex items-center justify-center gap-4">
-              <FiLogOut /> Logout
-            </Link>
-          </button>
-        </div>
-      </article>
-    </section>
+          </article>
+        </section>
+      )}
+    </>
   );
 };
 
