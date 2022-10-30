8e42ecd647b9c9704607bb1612f5d854a9241f40
Baaba <baabadampare@gmail.com>
2022-10-30 20:44:30 +0000

    add context

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
 import DashboardNav from "./DashboardNav";
+import { useStateValue } from "../context/stateProvider";
+import { FETCH_DATA } from "../utils";
 
 
 const Dashboard = ({ children }) => {
@@ -9,6 +11,30 @@ const Dashboard = ({ children }) => {
     setShowSideBar(!showSideBar);
   }
 
+  const [{}, disaptch] = useStateValue();
+  useEffect(() => {
+    FETCH_DATA("hotels", (data) => {
+      //to get actual hotels
+      console.log("hotels", data.data);
+      // store it in context state
+      disaptch({
+        type: "SET_HOTELS", // this is the action type to store hotels in /context/reducer.js
+        hotels: data.data, // now set hotels to data.data from the api
+      });
+    });
+    FETCH_DATA("rooms", (data) => {
+      //to get actual rooms
+      console.log("rooms", data.data);
+      // store it in context state
+      disaptch({
+        type: "SET_ROOMS", // this is the action type to store rooms in /context/reducer.js
+        rooms: data.data, // now set hotels to data.data from the api
+      });
+    });
+
+    // So in this useEffect Hook,
+    // we have fetched all rooms and hotels and dispatched the data to our context state. It is now avaiilable to use in all compnents without fetching from the API again.
+  }, []);
   return (
     <main className="flex h-screen justify-between">
        <SideBar showSideBar={showSideBar}/>
