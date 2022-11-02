f5da3a944c226401163d57fd8badc763cb66ca48
Baaba <baabadampare@gmail.com>
2022-11-02 00:01:26 +0000

    add animations to preloader

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../context/stateProvider";
 import { FETCH_DATA } from "../utils";
