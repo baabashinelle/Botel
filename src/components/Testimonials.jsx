5726ec4dfe5e313e8f235ac9578906ce114d6942
Baaba <baabadampare@gmail.com>
2022-11-02 13:32:21 +0000

    complete testimonial carousel

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
-import avatarImg from "../assets/avatar.jpg";
-
-// import avatarTwo from "../assets/room4.jpg";
 
 const Testimonials = () => {
   const changeSlide = () => {
@@ -11,13 +8,11 @@ const Testimonials = () => {
       slide[i].classList.toggle("slide-testimonial-active");
     }
 
-    // const avatarImg = document.querySelector(".avatar");
-    // avatarImg.src =
-    //   avatarImg.src == "../assets/avatar.jpg" ? "../assets/room4.jpg" : "../assets/avatar.jpg";
+    const avatarImg = document.querySelector(".img-div");
+    avatarImg.classList.toggle("new-bg");
   };
   return (
     <section className="bg-bg-off flex flex-col lg:flex-row justify-center items-center gap-16 pt-24 pb-20 p-3 xl:p-0 xl:py-14 font-text">
-        
         <div className="relative md:after:content-[url('./assets/planes.png')] after:block after:absolute xl:after:-right-[45em] lg:after:-right-[31em] md:after:-right-[9.5rem] lg:after:-top-[4em] md:after:top-[3em]">
           <article className="md:pt-16 xl:pt-0">
             <h3 className="font-header font-bold text-primary-b md:text-4xl sm:text-3xl text-2xl text-center md:text-left">
@@ -39,11 +34,9 @@ const Testimonials = () => {
         </div>
         <div className="flex flex-col md:pb-16 lg:pb-0 relative">
           <article className="sm:-mb-3 -mb-4 -ml-1 sm:-ml-5 z-30">
-            <img
-              src={avatarImg}
-              alt="avatar"
-              className="rounded-full w-16 h-16 avatar"
-            ></img>
+            <div
+              className="rounded-full w-16 h-16 avatar img-div transition-all"
+            ></div>
           </article>
           <article className="slide-testimonial md:w-[30rem] lg:w-[25rem] sm:w-[20rem] opacity-0 absolute top-14 drop-shadow-md slide-testimonial-active">
             <p className="text-base">
